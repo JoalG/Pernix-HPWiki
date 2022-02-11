e5079f972155314497575ca22e9ffbfb831c253d
Jose Gamboa <joalgamro2010@gmail.com>
2022-02-11 16:14:36 -0600

    A: Home component

---
+++ HP-WIKI/src/app/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CharactersService } from "../../services/characters.service";
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  characters!: any[];
+
+  constructor(private api:CharactersService){}
+
+  ngOnInit(): void {
+    this.api.apiCall().subscribe((data)=>{
+      this.characters = <any[]>data;
+    });
+  }
+
+}
