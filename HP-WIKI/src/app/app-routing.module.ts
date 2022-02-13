304a929c9c63af146724113136f2ae7457972724
Jose Gamboa <joalgamro2010@gmail.com>
2022-02-12 18:11:07 -0600

    A: Routing

--- HP-WIKI/src/app/app-routing.module.ts
+++ HP-WIKI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterInfoComponent } from './components/character-info/character-info.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'character-info/:id', component:CharacterInfoComponent},
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
